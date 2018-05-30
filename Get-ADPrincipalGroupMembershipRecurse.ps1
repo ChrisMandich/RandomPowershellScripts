cf3c77bd6a68d7398cd6db5547d2ad7dc2f9995d
Chris Mandich <Christopher.J.Mandich@sce.com>
2018-05-30 11:50:44 -0700

    Added Mandatory Flags
    Added Mandatory Flags

--- Get-ADPrincipalGroupMembershipRecurse.ps1
+++ Get-ADPrincipalGroupMembershipRecurse.ps1
@@ -3,7 +3,7 @@ function Get-ADPrincipalGroupMembershipRecurse {
 
 <#
 .SYNOPSIS
-This is a simple Powershell Script to retrieve all parents groups for an AD group. 
+This is a simple Powershell Script to retrieve all parents groups for an AD group.
 .DESCRIPTION
 The script uses Get-ADPrincipalGroupMembership to collect all parent groups for a group.
 .EXAMPLE
@@ -13,21 +13,21 @@ https://github.com/ChrisMandich/RandomPowershellScripts
 #>
 
     Param (
-        [Parameter(ValueFromPipeline=$true)]
+        [Parameter(ValueFromPipeline=$true,Mandatory=$true)]
         $Identity
     )
 
     $Identity = Get-ADGroup -Identity $Identity -ErrorAction Stop
-    
+
     function Add-ADPrincipalGroupList{
         Param (
             $Identity
         )
-        
-        #Write identity out and add it to list. 
+
+        #Write identity out and add it to list.
         Write-Output $Identity
         $Global:PrincipalGroupList += $Identity
-        
+
         #Check for groups in group
         $GROUP = Get-ADPrincipalGroupMembership -Identity $Identity.DistinguishedName | where objectClass -eq "group"
 
@@ -35,29 +35,29 @@ https://github.com/ChrisMandich/RandomPowershellScripts
         $GROUP | ForEach-Object{Get-ADPrincipalGroupMembershipRecurse -Identity $_.DistinguishedName}
     }
 
-    #check to see if the PrincipalGroupList has been created. If it has not been created, create and add identity. 
+    #check to see if the PrincipalGroupList has been created. If it has not been created, create and add identity.
     if($(Test-Path Variable:\PrincipalGroupList) -eq $false){
         #Create Global Variable
         $Global:PrincipalGroupList = @()
-        Add-ADPrincipalGroupList -Identity $Identity 
-                 
+        Add-ADPrincipalGroupList -Identity $Identity
+
     }
     elseif($Global:PrincipalGroupList.DistinguishedName.contains($Identity.DistinguishedName) -eq $false -and -not $(Test-Path Variable:\Identity) -eq $false){
-        #if group is not in list, add to PrincipalGroupList. 
-        Add-ADPrincipalGroupList -Identity $Identity 
+        #if group is not in list, add to PrincipalGroupList.
+        Add-ADPrincipalGroupList -Identity $Identity
 
         #Exit function
-        return;        
-    }   
+        return;
+    }
     else{
 
-        #Exit function 
+        #Exit function
         return;
-    }    
+    }
 
     #Remove variable
     Remove-Item Variable:\PrincipalGroupList
-    
+
     #Exit function
     Return;
 }
--- Get-AdGroupMemberRecurse.ps1
+++ Get-AdGroupMemberRecurse.ps1
@@ -3,9 +3,9 @@ function Get-AdGroupMemberRecurse {
 
 <#
 .SYNOPSIS
-This is a simple Powershell Script to retrieve all nested groups for an AD account. 
+This is a simple Powershell Script to retrieve all nested groups for an AD account.
 .DESCRIPTION
-The script uses Get-AdGroupMember to collect all associated groups for a parent group. The found groups are returned. 
+The script uses Get-AdGroupMember to collect all associated groups for a parent group. The found groups are returned.
 .EXAMPLE
 Get-AdGroupMemberRecurse -identity <Group Name>
 .LINK
@@ -13,21 +13,21 @@ https://github.com/ChrisMandich/RandomPowershellScripts
 #>
 
     Param (
-        [Parameter(ValueFromPipeline=$true)]
+        [Parameter(ValueFromPipeline=$true,Mandatory=$true)]
         $Identity
     )
 
     $Identity = Get-ADGroup -Identity $Identity
-    
+
     function Add-ADGroupList{
         Param (
             $Identity
         )
-        
-        #Write identity out and add it to list. 
+
+        #Write identity out and add it to list.
         Write-Output $Identity
         $Global:GroupList += $Identity
-        
+
         #Check for groups in group
         $GROUP = Get-ADGroupMember -Identity $Identity.DistinguishedName | where objectClass -eq "group"
 
@@ -35,29 +35,29 @@ https://github.com/ChrisMandich/RandomPowershellScripts
         $GROUP | ForEach-Object{Get-AdGroupMemberRecurse -Identity $_.DistinguishedName}
     }
 
-    #check to see if the grouplist has been created. If it has not been created, create and add identity. 
+    #check to see if the grouplist has been created. If it has not been created, create and add identity.
     if($(Test-Path Variable:\GroupList) -eq $false){
         #Create Global Variable
         $Global:GroupList = @()
-        Add-ADGroupList -Identity $Identity 
-                 
+        Add-ADGroupList -Identity $Identity
+
     }
     elseif($Global:GroupList.DistinguishedName.contains($Identity.DistinguishedName) -eq $false -and -not $(Test-Path Variable:\Identity) -eq $false){
-        #if group is not in list, add to grouplist. 
-        Add-ADGroupList -Identity $Identity 
+        #if group is not in list, add to grouplist.
+        Add-ADGroupList -Identity $Identity
 
         #Exit function
-        return;        
-    }   
+        return;
+    }
     else{
 
-        #Exit function 
+        #Exit function
         return;
-    }    
+    }
 
     #Remove variable
     Remove-Item Variable:\GroupList
-    
+
     #Exit function
     Return;
 }
